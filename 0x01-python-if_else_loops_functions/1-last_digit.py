23e2c3a577c7ebc0307c3872fd9a9ef243cd8e46
Maqobs <41755329+Maqobs@users.noreply.github.com>
2022-09-21 16:46:17 +0200

    single quotes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ if number < 0:
     print("last digit of {} is {} and ".format(number, digit), end='')
 
 if digit > 5:
-    print("is greater than 5")
+    print('is greater than 5')
 elif digit == 0:
-    print(" is 0")
+    print('is 0')
 else:
     print('is less than 6 and not 0')
