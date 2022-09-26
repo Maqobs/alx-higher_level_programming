3ea25637fb4f7e103d2d88f64038c75f348274f9
Maqobs <41755329+Maqobs@users.noreply.github.com>
2022-09-27 00:08:11 +0200

    day's work

---
+++ 0x03-python-data_structures/10-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python4
+divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
+
+my_list =[0, 1, 2, 3, 4, 45, 6]
+list_result = dividible_by_2(my_list)
+
+1 = 0
+while 1 < len(list_result):
+    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    i += 1
