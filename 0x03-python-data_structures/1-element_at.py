690d56c69a9f01341dcb473162b98ae941cf81bc
Maqobs <41755329+Maqobs@users.noreply.github.com>
2022-09-27 00:42:18 +0200

    unmmatched)

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-element_at == __import__('1-element_at').element_at
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-print("element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list) - 1:
+        return None
+    i = 0
+    while 1:
+        if i == idx:
+            return my_list[i]
+        i += 1
